26572dc1f149d3af7b05a3d7c29f31a4935b3bf3
Nic Sedlock <nicpic@gmail.com>
2026-09-22 17:02:11 -0400

    Added namespace Geom to all sections; removed variable (G : Geom) declaration, and added implicit {G : Geom} to all theorems. No longer need to call G explicitly except for three_dist and two_dist

--- Tarski/Basic.lean
+++ Tarski/Basic.lean
@@ -41,167 +41,162 @@ class Geom where
     congruent to a segment in the other triangle, then the fifth segments in both triangles must
     be congruent.
 
-    This is Euivalent to the side-angle-side rule for determining that two triangles are congruent;
+    This is equivalent to the side-angle-side rule for determining that two triangles are congruent;
     if the angles uxz and u'x'z' are congruent (there exist congruent triangles xuz and x'u'z'),
     and the two pairs of incident sides are congruent (xu ≡ x'u' and xz ≡ x'z'), then the remaining
     pair of sides is also congruent (uz ≡ u'z').
 -/
     sgmt_const (x y a b : Point) : ∃ z, B x y z ∧ E y z a b --beeson A4
 
--- Satz numbering based on Michael beeson's Tarski Formalization Project
+-- Satz numbering based on Michael Beeson's Tarski Formalization Project
 section Congruence
 
-variable (G : Geom)
-
+namespace Geom
 --Satz 2.1
-theorem Geom.E_refl {x y : G.Point} : E x y x y := G.E_eucl G.E_comm G.E_comm
+theorem E_refl {G : Geom} {x y : Point} : E x y x y := E_eucl E_comm E_comm
 
 --Satz 2.2
-theorem Geom.E_symm {x y z w : G.Point} : E x y z w → E z w x y :=
-    fun φ ↦ G.E_eucl φ G.E_refl
+theorem E.symm {G : Geom} {x y z w : Point} (h : E x y z w) : E z w x y :=
+    E_eucl h E_refl
 
-theorem Geom.E.symm {x y z w : G.Point} (h : E x y z w) : E z w x y := G.E_symm h
-
+theorem E_symm_iff {G : Geom} {x y z w : Point} : E x y z w ↔ E z w x y := by
+    constructor; all_goals exact fun x ↦ x.symm
 --Satz 2.3
-theorem Geom.E_trans {x y z u v w : G.Point} : E x y z w → E z w u v → E x y u v :=
-    fun φ ψ ↦ G.E_eucl (G.E_symm φ) ψ
+theorem E_trans {G : Geom} {x y z u v w : Point} : E x y z w → E z w u v → E x y u v :=
+    fun φ ψ ↦ E_eucl (φ.symm) ψ
 
 --Satz 2.5
-theorem Geom.E_flip_right {x y z w : G.Point} : E x y z w → E x y w z :=
-    fun φ ↦ G.E_trans φ G.E_comm
+theorem E_flip_right {G : Geom} {x y z w : Point} : E x y z w → E x y w z :=
+    fun φ ↦ E_trans φ E_comm
 
-theorem Geom.E.r {x y z w : G.Point} (h : E x y z w) : E x y w z := G.E_flip_right h
+theorem E.r {G : Geom} {x y z w : Point} (h : E x y z w) : E x y w z := E_flip_right h
 
-theorem Geom.E_flip_right_iff {x y z w : G.Point} : E x y z w ↔ E x y w z := by
-    constructor; all_goals exact G.E_flip_right
+theorem E_flip_right_iff {G : Geom} {x y z w : Point} : E x y z w ↔ E x y w z := by
+    constructor; all_goals exact E_flip_right
 
 --Satz 2.4
-theorem Geom.E_flip_left {x y z w : G.Point} : E x y z w → E y x z w :=
-    fun φ ↦ G.E_trans G.E_comm φ
+theorem E_flip_left {G : Geom} {x y z w : Point} : E x y z w → E y x z w :=
+    fun φ ↦ E_trans E_comm φ
 
-theorem Geom.E.l {x y z w : G.Point} (h : E x y z w) : E y x z w := G.E_flip_left h
+theorem E.l {G : Geom} {x y z w : Point} (h : E x y z w) : E y x z w := E_flip_left h
 
-theorem Geom.E_flip_left_iff {x y z w : G.Point} : E x y z w ↔ E y x z w := by
-    constructor; all_goals exact G.E_flip_left
+theorem E_flip_left_iff {G : Geom} {x y z w : Point} : E x y z w ↔ E y x z w := by
+    constructor; all_goals exact E_flip_left
 
 --Satz 2.14
-theorem Geom.E_flip_both {x y z w : G.Point} : E x y z w → E y x w z :=
-    fun φ ↦ G.E_flip_right <| G.E_flip_left φ
+theorem E_flip_both {G : Geom} {x y z w : Point} : E x y z w → E y x w z :=
+    fun φ ↦ E_flip_right <| E_flip_left φ
 
-theorem Geom.E.lr {x y z w : G.Point} (h : E x y z w) : E y x w z := h.l.r
+theorem E.lr {G : Geom} {x y z w : Point} (h : E x y z w) : E y x w z := h.l.r
 
-theorem Geom.E_flip_both_iff {x y z w : G.Point} : E x y z w ↔ E y x w z := by
-    constructor; all_goals exact G.E_flip_both
+theorem E_flip_both_iff {G : Geom} {x y z w : Point} : E x y z w ↔ E y x w z := by
+    constructor; all_goals exact E_flip_both
 
 --- Satz 2.8
-theorem Geom.degen_sgmts {x y : G.Point} : E x x y y := by
+theorem degen_sgmts {G : Geom} {x y : Point} : E x x y y := by
     have ⟨z, hz1, hz2⟩ : ∃ z, B x x z ∧ E x z y y := sgmt_const x x y y
     have hE : x = z := E_id hz2; rwa [←hE] at hz2
 
 --- Satz 2.11
-theorem Geom.sgmt_add {x y z x' y' z': G.Point} : B x y z → B x' y' z' →
+theorem sgmt_add {G : Geom} {x y z x' y' z' : Point} : B x y z → B x' y' z' →
     E x y x' y' → E y z y' z' → E x z x' z' := by
-    intro hb1 hb2 he1 he2; apply G.E_flip_both; by_cases h : x = y
+    intro hb1 hb2 he1 he2; apply E_flip_both; by_cases h : x = y
     ·   subst h; have he1 := he1.symm; have he2 := he2.symm
         have hx'y' : x' = y' := E_id he1; subst hx'y'; exact (he2.lr.symm)
-    apply G.five_sgmt h (hb1) hb2 he1 he2 (G.degen_sgmts) (he1.lr)
+    apply five_sgmt h (hb1) hb2 he1 he2 (degen_sgmts) (he1.lr)
 
 --- Satz 2.12
-theorem Geom.unique_sgmt_const {q a x b c y : G.Point} :
+theorem unique_sgmt_const {G : Geom} {q a x b c y : Point} :
     q ≠ a → B q a x → E a x b c → B q a y → E a y b c → x = y := by
         intro hne hb1 he1 hb2 he2
-        have h1 : E a x a y := G.E_trans he1 he2.symm
+        have h1 : E a x a y := E_trans he1 he2.symm
         have h2 : E x y y y :=
-            G.five_sgmt hne hb1 hb2 G.E_refl h1 G.E_refl G.E_refl
+            five_sgmt hne hb1 hb2 E_refl h1 E_refl E_refl
         exact E_id h2
-
+end Geom
 end Congruence
 
 section Betweenness
-
-variable (G : Geom)
-
-def Geom.Collinear (x y z : G.Point) := B x y z ∨ B y z x ∨ B z x y
-
+namespace Geom
 -- Satz 3.1
-theorem Geom.btwn_refl' {x y : G.Point} : B y x x := by
-    have ⟨a, ha, ha'⟩ := G.sgmt_const y x x x
-    have hax : x = a := G.E_id ha'; subst hax; exact ha
+theorem btwn_refl' {G : Geom} {x y : Point} : B y x x := by
+    have ⟨a, ha, ha'⟩ := sgmt_const y x x x
+    have hax : x = a := E_id ha'; subst hax; exact ha
 
 -- Satz 3.2
-theorem Geom.btwn_symm {x y z : G.Point} : B x y z → B z y x := by
-    intro h; have ⟨a, ha, ha'⟩ : ∃ a, B z a x ∧ B y a y := G.inner_pasch G.btwn_refl' h
-    have hay : y = a := G.btwn_id ha'; rwa [hay]
+theorem btwn_symm {G : Geom} {x y z : Point} : B x y z → B z y x := by
+    intro h; have ⟨a, ha, ha'⟩ : ∃ a, B z a x ∧ B y a y := inner_pasch btwn_refl' h
+    have hay : y = a := btwn_id ha'; rwa [hay]
 
-theorem Geom.B.symm {x y z : G.Point} (h : B x y z) : B z y x := G.btwn_symm h
+theorem B.symm {G : Geom}  {x y z : Point} (h : B x y z) : B z y x := btwn_symm h
 
-theorem Geom.btwn_symm_iff {x y z : G.Point} : B x y z ↔ B z y x := by
-    constructor; all_goals exact G.btwn_symm
+theorem btwn_symm_iff {G : Geom} {x y z : Point} : B x y z ↔ B z y x := by
+    constructor; all_goals exact btwn_symm
 
 -- Satz 2.15 : Proven in the book without symmetry but much easier with it
 
-theorem Geom.sgmt_add' {x y z x' y' z' : G.Point} :
+theorem sgmt_add' {G : Geom} {x y z x' y' z' : Point} :
     B x y z → B x' y' z' → E x y y' z' → E y z x' y' → E x z x' z' := by
-    intro hb1 hb2 he1 he2; rw [G.E_flip_right_iff] at he1 he2 ⊢
-    exact G.sgmt_add hb1 hb2.symm he1 he2
+    intro hb1 hb2 he1 he2; rw [E_flip_right_iff] at he1 he2 ⊢
+    exact sgmt_add hb1 hb2.symm he1 he2
 
 -- Satz 3.3
-theorem Geom.btwn_refl {x y : G.Point} : B x x y := G.btwn_symm G.btwn_refl'
+theorem btwn_refl {G : Geom} {x y : Point} : B x x y := btwn_symm btwn_refl'
 
 -- Satz 3.4
-theorem Geom.E_of_xyz_yxz {x y z : G.Point} : B x y z → B y x z → x = y := by
-    intro hb1 hb2; have ⟨a, ha1, ha2⟩ := G.inner_pasch hb1 hb2
-    exact (G.btwn_id ha1) ▸ (G.btwn_id ha2)
+theorem E_of_xyz_yxz {G : Geom} {x y z : Point} : B x y z → B y x z → x = y := by
+    intro hb1 hb2; have ⟨a, ha1, ha2⟩ := inner_pasch hb1 hb2
+    exact (btwn_id ha1) ▸ (btwn_id ha2)
 
 -- Satz 3.5a
-theorem Geom.btwn_xyz_of_xyw_yzw {x y z w : G.Point} : B x y w → B y z w → B x y z := by
-    intro hb1 hb2; have ⟨a, ha1, ha2⟩ := G.inner_pasch hb1 hb2
-    rw [G.btwn_id ha1]; apply G.btwn_symm ha2
+theorem btwn_xyz_of_xyw_yzw {G : Geom} {x y z w : Point} : B x y w → B y z w → B x y z := by
+    intro hb1 hb2; have ⟨a, ha1, ha2⟩ := inner_pasch hb1 hb2
+    rw [btwn_id ha1]; apply btwn_symm ha2
 
 -- Satz 3.6a
-theorem Geom.btwn_yzw_of_xyz_xzw {x y z w : G.Point} : B x y z → B x z w → B y z w :=
-    fun hb1 hb2 ↦ (G.btwn_xyz_of_xyw_yzw hb2.symm hb1.symm).symm
+theorem btwn_yzw_of_xyz_xzw {G : Geom} {x y z w : Point} : B x y z → B x z w → B y z w :=
+    fun hb1 hb2 ↦ (btwn_xyz_of_xyw_yzw hb2.symm hb1.symm).symm
 
 -- Satz 3.7a
-theorem Geom.btwn_xzw_of_xyz_yzw_ne {x y z w} : B x y z → B y z w → y ≠ z → B x z w := by
+theorem btwn_xzw_of_xyz_yzw_ne {G : Geom} {x y z w} : B x y z → B y z w → y ≠ z → B x z w := by
     intro hb1 hb2 hne; have ⟨a, ha1, ha2⟩ : ∃ a, B x z a ∧ E z a z w := sgmt_const x z z w
-    have hb3 : B y z a := btwn_yzw_of_xyz_xzw G hb1 ha1
-    have he1 : E w a a a := G.five_sgmt hne hb2 hb3 (G.E_refl) (ha2).symm (G.E_refl) (G.E_refl)
-    exact (G.E_id he1) ▸ ha1
+    have hb3 : B y z a := btwn_yzw_of_xyz_xzw hb1 ha1
+    have he1 : E w a a a := five_sgmt hne hb2 hb3 (E_refl) (ha2).symm (E_refl) (E_refl)
+    exact (E_id he1) ▸ ha1
 
 -- Satz 3.5b
-theorem Geom.btwn_xzw_of_xyw_yzw {x y z w : G.Point} : B x y w → B y z w → B x z w := by
-    intro hb1 hb2; have h := G.btwn_xyz_of_xyw_yzw hb1 hb2
+theorem btwn_xzw_of_xyw_yzw {G : Geom} {x y z w : Point} : B x y w → B y z w → B x z w := by
+    intro hb1 hb2; have h := btwn_xyz_of_xyw_yzw hb1 hb2
     by_cases h' : y = z
     · exact h' ▸ hb1
-    · exact G.btwn_xzw_of_xyz_yzw_ne h hb2 h'
+    · exact btwn_xzw_of_xyz_yzw_ne h hb2 h'
 
 -- Satz 3.6b
-theorem Geom.btwn_xyw_of_xyz_xzw {x y z w : G.Point} : B x y z → B x z w → B x y w :=
-    fun hb1 hb2 ↦ (G.btwn_xzw_of_xyw_yzw hb2.symm hb1.symm).symm
+theorem btwn_xyw_of_xyz_xzw {G : Geom} {x y z w : Point} : B x y z → B x z w → B x y w :=
+    fun hb1 hb2 ↦ (btwn_xzw_of_xyw_yzw hb2.symm hb1.symm).symm
 
 -- Satz 3.7b
-theorem Geom.btwn_xyw_of_xyz_yzw_ne {x y z w} : B x y z → B y z w → y ≠ z → B x y w := by
-    intro hb1 hb2 hne; have hb3 : B x z w := G.btwn_xzw_of_xyz_yzw_ne hb1 hb2 hne
-    exact G.btwn_xyw_of_xyz_xzw hb1 hb3
+theorem btwn_xyw_of_xyz_yzw_ne {G : Geom} {x y z w} : B x y z → B y z w → y ≠ z → B x y w := by
+    intro hb1 hb2 hne; have hb3 : B x z w := btwn_xzw_of_xyz_yzw_ne hb1 hb2 hne
+    exact btwn_xyw_of_xyz_xzw hb1 hb3
 
 -- Satz 3.13abc, essentially.
 /-  In Beeson's work, the a b c in lo_dim are constants that
     just exist as part of the Skolemization; these three results show that a b and c are distinct.
     We show the more general result that if y is not on the interval xz, then y is not
     Eual to either x nor z , and that this implies the existence of two and three distinct points-/
-theorem Geom.dist_of_not_btwn {x y z : G.Point} : ¬ B x y z → x ≠ y ∧ y ≠ z := by
+theorem dist_of_not_btwn {G : Geom} {x y z : Point} : ¬ B x y z → x ≠ y ∧ y ≠ z := by
     intro h; refine ⟨?_, ?_⟩
-    · intro h'; subst h'; exact h G.btwn_refl
-    · intro h'; subst h'; exact h G.btwn_refl'
+    · intro h'; subst h'; exact h btwn_refl
+    · intro h'; subst h'; exact h btwn_refl'
 
-theorem Geom.three_dist : ∃ a b c : G.Point, a ≠ b ∧ b ≠ c ∧ a ≠ c := by
-    have ⟨a, b, c, h1, h2, _⟩ := G.lo_dim; refine ⟨a, b, c, ?_, ?_, ?_⟩
-    · exact (G.dist_of_not_btwn h1).1
-    · exact (G.dist_of_not_btwn h1).2
-    · exact (G.dist_of_not_btwn h2).2.symm
+theorem three_dist (G : Geom) : ∃ a b c : Point, a ≠ b ∧ b ≠ c ∧ a ≠ c := by
+    have ⟨a, b, c, h1, h2, _⟩ := lo_dim; refine ⟨a, b, c, ?_, ?_, ?_⟩
+    · exact (dist_of_not_btwn h1).1
+    · exact (dist_of_not_btwn h1).2
+    · exact (dist_of_not_btwn h2).2.symm
 
-theorem Geom.two_dist : ∃ a b : G.Point, a ≠ b := by
+theorem two_dist (G : Geom) : ∃ a b : Point, a ≠ b := by
     have ⟨a, b, _, h, _⟩ := G.three_dist; exact ⟨a, b, h⟩
 
 -- Satz 3.14ab, essentially
@@ -211,24 +206,30 @@ theorem Geom.two_dist : ∃ a b : G.Point, a ≠ b := by
     the definition of sgmt_const so I'm skipping it; the only content here for us is that if
     a ≠ b and E x y a b, then x ≠ y. -/
 
-theorem Geom.E_id_mt {x y a b : G.Point} : a ≠ b → E x y a b → x ≠ y :=
-    fun hE1 he1 hE2 ↦ hE1 (G.E_id (hE2 ▸ he1.symm))
+theorem E_id_mt {G : Geom} {x y a b : Point} : a ≠ b → E x y a b → x ≠ y :=
+    fun hE1 he1 hE2 ↦ hE1 (E_id (hE2 ▸ he1.symm))
+
+theorem sgmt_const' {G : Geom} (x y : Point) : ∃ z, B x y z ∧ y ≠ z := by
+    have ⟨a, b, hab⟩ := G.two_dist;
+    have ⟨z, h1, h2⟩ := sgmt_const x y a b
+    exact ⟨z, h1, E_id_mt hab h2⟩
 
 -- Satz 3.17
 /-  Suppose you have a triangle A B C, with points X on AB, Y on BC, and Z on AC. Then the line
     segments ZB and XY intersect at some point P; basically the converse of ax_euclid
     update : I'm an idiot, this is the crossbar theorem -/
-theorem Geom.crossbar {a b c x y z : G.Point} :
+theorem crossbar {G : Geom} {a b c x y z : Point} :
     B a x b → B c y b → B a z c → (∃ p, B z p b ∧ B x p y) := by
-    intro hb1 hb2 hb3; have ⟨e, hb4, hb5⟩ := G.inner_pasch hb2.symm hb3
+    intro hb1 hb2 hb3; have ⟨e, hb4, hb5⟩ := inner_pasch hb2.symm hb3
     have ⟨p, hb6, hb7⟩ := inner_pasch hb1.symm hb4
-    refine ⟨p, ?_, hb6⟩; apply G.btwn_xzw_of_xyw_yzw hb5 hb7
+    refine ⟨p, ?_, hb6⟩; apply btwn_xzw_of_xyw_yzw hb5 hb7
 
+end Geom
 end Betweenness
 
 section Ch4
 
-variable (G : Geom)
+namespace Geom
 /-
     Notes about Chapter 4: There is a function called insert: What it is doing is
     given a ray a'c', constructing a point b' on it so that ab ≡ a'b'.
@@ -240,32 +241,106 @@ variable (G : Geom)
 
 -/
 
--- this is definition 4.3 in Beeson (basically side side side premises?)
-def E3 {G : Geom} {x y z a b c : G.Point} := G.E x y a b ∧ G.E y z b c ∧ G.E x z a c
-
--- this is definition 4.10 in Beeson
-def Col {G : Geom} {x y z : G.Point} := G.B x y z ∨ G.B y z x ∨ G.B z x y
-
-
 -- Satz 4.2; this is basically SSS to five_sgmt's SAS
 /- This is actually very clever so I feel the need to explain it: The way it works is
 we know xz = x'z', xu=x'u' and zu = z'u' so we construct w and w' so that B xzw and
 B x'z'w', use five_sgmt to get wu = w'u', and then use it again backwards on the
 triangles w y u and w' y' u' to get yu = y'u'.-/
-theorem Geom.inner_five_sgmt {x y z u x' y' z' u' : G.Point} : B x y z → B x' y' z' →
+theorem inner_five_sgmt {G : Geom} {x y z u x' y' z' u' : Point} : B x y z → B x' y' z' →
     E x y x' y' → E y z y' z' → E x u x' u' → E z u z' u' → E y u y' u' := by
     intro hb1 hb'1 he1 he2 he3 he4
     by_cases heq : x = z
-    ·   subst heq; rw [btwn_id hb1] at he1 he3
-        rwa [G.E_id he1.symm] at he3
-    have ⟨w, hw1, hw2⟩ := G.sgmt_const x z x z
-    have ⟨w', hw'1, hw'2⟩ := G.sgmt_const x' z' x' z'
-    have he5 : E x z x' z' := G.sgmt_add hb1 hb'1 he1 he2
-    have he6 : E z w z' w' := G.E_trans (G.E_trans hw2 he5) hw'2.symm
-    have he7 : E w u w' u' := G.five_sgmt heq hw1 hw'1 he5 he6 he3 he4
-    have heq2 : w ≠ z := Ne.symm (G.E_id_mt heq hw2)
-    have hb2 : B w z y := (G.btwn_yzw_of_xyz_xzw hb1 hw1).symm
-    have hb'2 : B w' z' y' := (G.btwn_yzw_of_xyz_xzw hb'1 hw'1).symm
-    apply G.five_sgmt heq2 hb2 hb'2 he6.lr he2.lr he7 he4
+    ·   subst heq; rw [btwn_id hb1] at he1 he3; rwa [E_id he1.symm] at he3
+    have ⟨w, hw1, heq2⟩ := sgmt_const' x z
+    have ⟨w', hw'1, he6⟩ := sgmt_const x' z' z w
+    have he5 : E x z x' z' := sgmt_add hb1 hb'1 he1 he2
+    have he7 : E w u w' u' := five_sgmt heq hw1 hw'1 he5 he6.symm he3 he4
+    have hb2 : B w z y := (btwn_yzw_of_xyz_xzw hb1 hw1).symm
+    have hb'2 : B w' z' y' := (btwn_yzw_of_xyz_xzw hb'1 hw'1).symm
+    apply five_sgmt heq2.symm hb2 hb'2 he6.lr.symm he2.lr he7 he4
 
+-- Satz 4.3: segment subtraction
+/-  Same trick as above; if y = z we're done, so assume y ≠ z and extend ray yz
+    and ray y'z' to w and w', then use five_sgmt with w z y x and w' z' y' x' -/
+
+theorem sgmt_sub {G : Geom} {x y z x' y' z' : Point} : B x y z → B x' y' z' →
+    E x z x' z' → E y z y' z' → E x y x' y' := by
+    intro hb1 hb'1 he1 he2; by_cases heq : y = z
+    · subst heq; exact (E_id he2.symm) ▸ he1
+    have heq' : y' ≠ z' := E_id_mt heq he2.symm
+    have ⟨w, hb2, heq2⟩ := sgmt_const' y z
+    have ⟨w', hb'2, he3⟩ := sgmt_const y' z' z w
+    rw [E_symm_iff] at he3
+    have hb3 : B x z w := btwn_xzw_of_xyz_yzw_ne hb1 hb2 heq
+    have hb'3 : B x' z' w' := btwn_xzw_of_xyz_yzw_ne hb'1 hb'2 heq'
+    have he4 : E x w x' w' := sgmt_add hb3 hb'3 he1 he3
+    rw [btwn_symm_iff] at hb2 hb'2
+    rw [E_flip_both_iff] at he1 he2 he3 he4 ⊢
+    apply five_sgmt heq2.symm hb2 hb'2 he3 he2 he4 he1
+
+
+-- this is definition 4.3 in Beeson (basically side side side premises?)
+def E3 {G : Geom} (x y z a b c : Point) := E x y a b ∧ E y z b c ∧ E x z a c
+
+-- Satz 4.5 and 4.6
+/-  if ac ≡ a'c' and B a b c there is some b' so that B a' b' c'
+    and ab ≡ a'b' and bc ≡ b'c'; 4.6 shows that you don't need to know what c is
+    to construct b' but this is not interesting to me; Beeson uses a function
+    E3 here to capture all the congruences but I think this way is easier in
+    Lean-/
+theorem sgmt_split {G : Geom} {a b c a' c' : Point} : B a b c → E a c a' c' →
+    (∃ b', B a' b' c' ∧ E a b a' b' ∧ E b c b' c') := by
+        intro hb1 he1; have ⟨w', hb'2, heq2⟩ := sgmt_const' c' a'
+        have ⟨w, hb2, he2⟩ := sgmt_const c a a' w'
+        have ⟨b', hb'3, he3⟩ := sgmt_const w' a' a b
+        have ⟨c'', hb'4, he4⟩ := sgmt_const w' b' b c
+        have hb'5 : B a' b' c'' := btwn_yzw_of_xyz_xzw hb'3 hb'4
+        have he5 : E a c a' c'' := sgmt_add hb1 hb'5 he3.symm he4.symm
+        have he6 : E a' c' a' c'' := E_eucl he1 he5
+        have hb'6 : B w' a' c'' := btwn_xyw_of_xyz_xzw hb'3 hb'4
+        have heq3 : c' = c'' := unique_sgmt_const heq2.symm hb'2.symm (E_refl) hb'6 he6.symm
+        subst heq3; exact ⟨b', hb'5, he3.symm, he4.symm⟩
+
+-- this is definition 4.10 in Beeson
+def Col {G : Geom} (x y z : Point) := B x y z ∨ B y z x ∨ B z x y
+
+-- Satz 4.11 : Collinear is symmetric in all of its inputs
+theorem Col.xy {G : Geom} {x y z : Point} (h : Col x y z) : Col y x z := by
+    unfold Col at h ⊢; rcases h with h' | h' | h'
+    ·   exact Or.inr <| Or.inr h'.symm
+    ·   exact Or.inr <| Or.inl h'.symm
+    ·   exact Or.inl h'.symm
+
+theorem Col.xz {G : Geom} {x y z : Point} (h : Col x y z) : Col z y x := by
+    unfold Col at h ⊢; rcases h with h' | h' | h'
+    ·   exact Or.inl h'.symm
+    ·   exact Or.inr <| Or.inr h'.symm
+    ·   exact Or.inr <| Or.inl h'.symm
+
+theorem Col.yz {G : Geom} {x y z : Point} (h : Col x y z) : Col x z y := h.xz.xy.xz
+
+theorem Col.l {G : Geom} {x y z : Point} {h : Col x y z} : Col y z x := h.xy.yz
+
+theorem Col.r {G : Geom} {x y z : Point} {h : Col x y z} : Col z x y := h.xz.yz
+
+theorem col_xy_iff {G : Geom} {x y z : Point} : Col x y z ↔ Col y x z := by
+    constructor; all_goals exact fun x ↦ x.xy
+
+theorem col_xz_iff {G : Geom} {x y z : Point} : Col x y z ↔ Col z y x := by
+    constructor; all_goals exact fun x ↦ x.xz
+
+theorem col_yz_iff {G : Geom} {x y z : Point} : Col x y z ↔ Col x z y := by
+    constructor; all_goals exact fun x ↦ x.yz
+
+theorem col_shift_iff {G : Geom} {x y z : Point} : Col x y z ↔ Col y z x := by
+    constructor; exact fun x ↦ x.l; exact fun x ↦ x.r
+
+-- Satz 4.12ab
+theorem col_triv_xxy {G : Geom} {x y : Point} : Col x x y := by
+    unfold Col; exact Or.inl (btwn_refl)
+
+theorem col_triv_xyx {G : Geom} {x y : Point} : Col x y x := by
+    unfold Col; exact Or.inr (Or.inr btwn_refl)
+
+end Geom
 end Ch4
